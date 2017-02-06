701e9df2984b84e2741b756d5da038051a0768f1
biagiocaldararo <biagiocaldararo@live.it>
2017-02-06 14:02:43 +0100

    Update script.sh

--- script/script.sh
+++ script/script.sh
@@ -9,10 +9,10 @@ python classifica_compl.py
 
 sudo service  mongod start
 
-mongoimport --db test --collection classifica --drop --file ~/Documenti/VirtualCoach/script/classifica_compl.json
-mongoimport --db test --collection probabili --drop --file ~/Documenti/VirtualCoach/script/probabili.json
-mongoimport --db test --collection quotazioni --drop --file ~/Documenti/VirtualCoach/script/quotazioni.json
-mongoimport --db test --collection statistiche --drop --file ~/Documenti/VirtualCoach/script/statistiche.json
+mongoimport --db test --collection classifica --drop --file ~/<Your-Path-Here>/VirtualCoach/script/classifica_compl.json
+mongoimport --db test --collection probabili --drop --file ~/<Your-Path-Here>/VirtualCoach/script/probabili.json
+mongoimport --db test --collection quotazioni --drop --file ~/<Your-Path-Here>/VirtualCoach/script/quotazioni.json
+mongoimport --db test --collection statistiche --drop --file ~/<Your-Path-Here>/VirtualCoach/script/statistiche.json
 
 
 
