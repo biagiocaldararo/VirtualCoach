5243e4e0cde0554ed9c5114be32296e06ce633c0
Biagio <biagiocaldararo@live.it>
2017-03-02 07:36:36 +0100

    rimuovi rosa ora rimuove anche formazione e riserve

--- src/main/java/com/virtualcoach/action/AzioneRimuoviRosa.java
+++ src/main/java/com/virtualcoach/action/AzioneRimuoviRosa.java
@@ -8,6 +8,8 @@ public class AzioneRimuoviRosa extends Azione {
 	@Override
 	public String esegui(HttpServletRequest request) throws ServletException {
 		request.getSession().removeAttribute("rosa");
+		request.getSession().removeAttribute("formazione");
+		request.getSession().removeAttribute("riserve");
 		return "rimuovi_rosa";
 	}
 }
